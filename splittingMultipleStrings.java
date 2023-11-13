f1f38e4653736ae27225faa82754689159ba432b
dzlagit <146720843+dzlagit@users.noreply.github.com>
2023-11-13 13:44:19 +0000

    Update splittingMultipleStrings.java

--- splittingMultipleStrings.java
+++ splittingMultipleStrings.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a sentence (end in .) and a word: ");
+        System.out.println("Enter multiple sentences split by full stops: ");
         String userInput = input.nextLine();
 
         String[] sentences = userInput.split("\\.");
         
         for (int i = 0; i < sentences.length; i++) {
             String[] words = sentences[i].split("\\s+");
+            System.out.println("Words in sentence " + (i + 1) + ":");
             System.out.println(sentences[i]);
         }
     }
